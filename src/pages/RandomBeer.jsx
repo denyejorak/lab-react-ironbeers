2f947fc93d12d7e705f76634eddc78e4621d86b8
denyejorak <karolyenyed@gmail.com>
2023-07-26 12:04:11 +0200

    done

---
+++ src/pages/RandomBeer.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+function RandomBeer() {
+  return (
+    <div className="food-box">
+      <Header />
+    </div>
+  );
+}
+
+export default RandomBeer;
